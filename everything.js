6c4ad8f532a2dab0b4faf0a4f698fc2290c6de54
sank108 <137850649+sank108@users.noreply.github.com>
2023-12-05 20:54:39 +0530

    Done with slider logic

--- everything.js
+++ everything.js
@@ -1,34 +1,100 @@
 const rangeInput = document.querySelectorAll(".range-input input");
 const progress = document.querySelector(".slider .progress1");
 const progress1 = document.querySelector(".slider .progress2");
-
+const minmax = document.getElementById("#minmax");
+const slider = document.getElementsByClassName('.slider');
+const priceInput = document.querySelectorAll(".price-range input");
+const activeFilter = document.querySelector(".active-filter");
+const pricepara = document.querySelector(".specific-price-para");
+const clear = document.querySelector(".clr-btn");
 
 let priceGap = 10;
-rangeInput.forEach(input =>{
-    input.addEventListener("input" , (e)=>{
-        let minVal = parseInt(rangeInput[0].value);
-        let maxVal = parseInt(rangeInput[1].value);
-        console.log(minVal,maxVal);
-        
-        if(maxVal - minVal < priceGap){
-            if(e.target.className === "range-min"){
-                rangeInput[0].value = maxVal - priceGap;
+// 
+let minVal;
+let maxVal;
+    rangeInput.forEach(input =>{
+        input.addEventListener("input" , (e)=>{
+            minVal = parseInt(rangeInput[0].value);
+            maxVal = parseInt(rangeInput[1].value);
+            // console.log(minVal,maxVal);
+            
+            if(maxVal - minVal < priceGap){
+                if(e.target.className === "range-min"){
+                    rangeInput[0].value = maxVal - priceGap;
+                }
+                else{
+                    rangeInput[1].value = minVal + priceGap;
+                }
+               
+            }else{
+                // priceInput.value = minVal;
+                priceInput[0].value = "£"+ minVal;
+                priceInput[1].value = "£"+ maxVal;
+    
+                
+                progress.style.left = ((minVal)/ rangeInput[0].max) *100 + "%";
+                progress1.style.left = ((maxVal - 80) / rangeInput[1].max) *100 + "%";
             }
-            else{
-                rangeInput[1].value = minVal + priceGap;
+    
+            console.log(activeFilter.display);
+            minVal = parseInt(rangeInput[0].value);
+            maxVal = parseInt(rangeInput[1].value);
+            // if(minVal != 0 || maxVal != 40){
+            //     activeFilter.classList.add("active");
+            // }else{
+            //     activeFilter.classList.remove("active");
+            // }
+            if(minVal == 0 && maxVal == 40){
+                pricepara.textContent = "No Filter Applied";
             }
-           
-        }else{
-            progress.style.left = ((minVal)/ rangeInput[0].max) *100 + "%";
-            progress1.style.left = ((maxVal - 80) / rangeInput[1].max) *100 + "%";
-        }
-
-
-        
-        // let pro = (maxVal / rangeInput[1].max) *100 + "%";
-        // console.log(pro);
-
-
-        
+            else if(minVal >= 0 && maxVal == 40){
+                pricepara.textContent = "Up to £" + minVal;
+            }else if(minVal == 0 && maxVal <= 40){
+                pricepara.textContent = "Up to £" + maxVal;
+            }else if(minVal != 0 && maxVal != 40){
+                pricepara.textContent = "Between £" + minVal + " and £" + maxVal;
+            }
+            
+            
+            // let pro = (maxVal / rangeInput[1].max) *100 + "%";
+            // console.log(pro);
+    
+            // console.log(,priceInput[1].value);
+            
+        });
     });
-});
+    
+
+
+// rangeInput.forEach(input =>{
+//     input.addEventListener("input" , (e)=>{
+//         rangeInput.addEventListener('dragstart', function (event) {
+//             // The drag has started
+//             console.log('Drag started');
+//         });
+        
+//         rangeInput.addEventListener('drag', function (event) {
+//             // The element is being dragged
+//             console.log('Dragging');
+//         });
+        
+//         rangeInput.addEventListener('dragend', function (event) {
+//             // The drag has ended
+//             console.log('Drag ended');
+//         });
+//     });
+// });
+
+clear.addEventListener("click",(e) =>{
+    if(minVal != 0 || maxVal != 40){
+        rangeInput[0].value = 0;
+        rangeInput[1].value = 40;
+        priceInput[0].value = "£0";
+        priceInput[1].value = "£40";
+        progress.style.left = 0;
+        progress1.style.left = 0;
+        pricepara.textContent = "No Filter Applied";
+    }
+})
+
+
